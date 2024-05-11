09c755410bac5b0482253249b503599dda5f41fc
Yuri Andriaccio <yurand2000@gmail.com>
2024-05-12 00:40:10 +0200

    Add spin and lazy_static crates, Implement fmt functions for the VGA buffer

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,17 @@ pub mod vga_buffer;
 use core::arch::asm;
 use core::panic::PanicInfo;
 
-static HELLO: &str = "Hello World!";
-
 #[no_mangle]
 pub extern "C" fn kernel_start() -> ! {
-    let mut writer = vga_buffer::Writer::new();
-    writer.write_line(HELLO);
+    println!("Hello World!");
 
     unsafe { asm!("hlt"); }
-    panic!();
+    loop {}
 }
 
 #[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
+fn panic(info: &PanicInfo<'_>) -> ! {
+    println!("Kernel panic: {}", info);
+    unsafe { asm!("hlt"); }
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -119,3 +119,34 @@ impl Writer {
         self.write(str);
     }
 }
+
+impl core::fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        self.write(s);
+        Ok(())
+    }
+}
+
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer::new());
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
